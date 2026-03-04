8318a6651d18b3c4df2286e031231011cc2db9c2
asli0171 <aslifarrah@gmail.com>
2026-03-04 07:12:54 +0100

    added entity classes

--- src/main/java/dk/kinoxp/kinoxp/model/Film.java
+++ src/main/java/dk/kinoxp/kinoxp/model/Film.java
@@ -1,4 +1,70 @@
 package dk.kinoxp.kinoxp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "FILM")
 public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String language;
+
+    private String agerating;
+
+    private int length;
+
+    public Film() {}
+
+    public Film(String title, String language, String agerating, int length ) {
+        this.setTitle(title);
+        this.setLanguage(language);
+        this.setAgerating(agerating);
+        this.setLength(length);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle (){
+        return title;
+    }
+
+    public void setTitle (String title){
+        this.title = title;
+
+    }
+
+    public String getLanguage (){
+        return language;
+
+    }
+
+    public void setLanguage (String language){
+        this.language = language;
+    }
+
+    public String getAgerating (){
+        return agerating;
+    }
+
+    public void setAgerating (String agerating){
+        this.agerating = agerating;
+
+    }
+
+    public int getLength (){
+        return length;
+
+    }
+
+    public void setLength (int length){
+        this.length = length;
+    }
 }
+
--- src/main/java/dk/kinoxp/kinoxp/model/Seat.java
+++ src/main/java/dk/kinoxp/kinoxp/model/Seat.java
@@ -1,4 +1,43 @@
 package dk.kinoxp.kinoxp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "SEAT")
 public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String seatType;
+
+    private int seatNumber;
+
+    public Seat() {}
+
+    public Seat(String seatType, int seatNumber) {
+        this.setSeatType(seatType);
+        this.setSeatNumber(seatNumber);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSeatType (){
+        return seatType;
+    }
+
+    public void setSeatType (String seatType){
+        this.seatType = seatType;
+    }
+
+    public int getSeatNumber (){
+        return seatNumber;
+    }
+
+    public void setSeatNumber (int seatNumber){
+        this.seatNumber = seatNumber;
+    }
 }
