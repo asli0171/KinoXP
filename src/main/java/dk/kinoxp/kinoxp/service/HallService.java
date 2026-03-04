cf60f97859dac91731e36122f6099de2be1e9eb5
asli0171 <aslifarrah@gmail.com>
2026-03-04 11:08:57 +0100

    added services

--- src/main/java/dk/kinoxp/kinoxp/service/HallService.java
+++ src/main/java/dk/kinoxp/kinoxp/service/HallService.java
@@ -1,4 +1,32 @@
 package dk.kinoxp.kinoxp.service;
 
+import dk.kinoxp.kinoxp.repository.HallRepository;
+import org.springframework.stereotype.Service;
+import dk.kinoxp.kinoxp.model.Hall;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class HallService {
+    private final HallRepository hallRepository;
+
+    public HallService(HallRepository hallRepository){
+        this.hallRepository = hallRepository;
+    }
+
+    public Hall savehall (Hall hall) {
+        return hallRepository.save(hall);
+    }
+
+    public List<Hall> getAllHalls (){
+        return hallRepository.findAll();
+    }
+
+    public Optional<Hall> getHallById (Long id){
+        return hallRepository.findById(id);
+    }
+
+    public void deleteHallById (Long id){
+        hallRepository.deleteById(id);
+    }
 }
